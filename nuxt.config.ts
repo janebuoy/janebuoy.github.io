f967c415ad7822fa6d494585ee2e152dcbecc6c0
mugraph <micrograph@posteo.de>
2024-09-08 13:57:45 +0200

    chore: update deps and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,42 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/tailwindcss',
-		'@nuxtjs/color-mode'
-  ],
-	app: {
-		head: {
-				link: [{ rel: 'icon', type: 'image/png', href: '/favicon/favicon-32x32.png' }]
-		},
+ ssr: true,
+
+ modules: [
+   '@nuxt/content',
+   '@nuxtjs/tailwindcss',
+       '@nuxtjs/color-mode'
+ ],
+
+ app: {
+     head: {
+             link: [{ rel: 'icon', type: 'image/png', href: '/favicon/favicon-32x32.png' }]
+     },
 	},
-  vue: {
-    compilerOptions: {
-      isCustomElement: tag => ['author'].includes(tag)
-    }
-  },
-  nitro: {
-    prerender: {
-      routes: ['/words/feed', '/words/json', '/words/atom'],
-    },
-  },
-  content: {
-    documentDriven: false,
-    highlight: {
-      preload: ['diff', 'go', 'json', 'js', 'ts', 'css', 'shell', 'html', 'md', 'yaml'],
-      theme: {
-        default: 'github-light',
-        dark: 'github-dark',
-        sepia: 'monokai'
-      }
-    },
-    experimental: {
-      clientDB: true,
-      stripQueryParameters: true
-    }
-  },
-	colorMode: {
-		classSuffix: ''
-	}
+
+ vue: {
+   compilerOptions: {
+     isCustomElement: tag => ['author'].includes(tag)
+   }
+ },
+
+ nitro: {
+   prerender: {
+     routes: ['/words/feed', '/words/json', '/words/atom'],
+   },
+ },
+
+ content: {
+   documentDriven: false,
+   highlight: {
+     preload: ['diff', 'go', 'json', 'js', 'ts', 'css', 'shell', 'html', 'md', 'yaml', 'python'],
+     theme: {
+       default: 'github-light',
+       dark: 'github-dark',
+       sepia: 'monokai'
+     }
+   }
+ },
+
+ colorMode: {
+     classSuffix: '',
+	},
+
+ compatibilityDate: '2024-09-08'
 })
