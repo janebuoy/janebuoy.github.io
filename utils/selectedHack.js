e47e8f984d28a8adb98f7f067cd2f310f6ba77f8
mugraph <micrograph@posteo.de>
2024-09-08 13:57:23 +0200

    fix: nav selection and borders hack

--- utils/selectedHack.js
+++ utils/selectedHack.js
@@ -1,23 +1,24 @@
 export function selectedClass(route, post) {
-  const currentHash = route.hash.slice(1);
-  const currentHref = route.href.slice(1).split('?')[0].split('/')[0];
+  const currentHash = route?.hash?.slice(1);
+  const currentHref = route?.href?.slice(1).split('?')[0].split('/')[0];
+  const currentPath = route?.fullPath?.slice(1).split('/')[0]
   const postPath = post._path.slice(1);
 
-return currentHash === postPath || currentHref === postPath
+return [currentHash, currentHref, currentPath].includes(postPath)
 };
 
 export function selectedClassCard(route, post) {
-  const currentHash = route.hash.slice(1);
-  const currentHref = route.href.slice(1).split('?')[0].split('#')[0];
+  const currentHash = route?.hash?.slice(1);
+  const currentHref = route?.href?.slice(1).split('?')[0].split('#')[0];
   const postPath = post._path.slice(1);
 
 return currentHash === postPath || currentHref === postPath
 };
 
 export const borderClasses = (route, pathArr, index) => {
-  const currentHash = route.hash.slice(1);
-  const currentHref = route.href.slice(1).split('?')[0].split('/')[0];
+  const currentHash = route?.hash?.slice(1);
+  const currentHref = route?.href?.slice(1).split('?')[0].split('/')[0];
+  const currentPath = route?.fullPath?.slice(1).split('/')[0]
 
-  return pathArr.indexOf(currentHash) === index + 1 ||
-    pathArr.indexOf(currentHref) === index + 1
+  return [currentHash, currentHref, currentPath].some(value => pathArr.indexOf(value) === index + 1);
 };
